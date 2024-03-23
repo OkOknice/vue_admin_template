71f07af99cdba4e895e247ef4e3eb99a4d9c8a8b
wuzhaoxian <okokwu0219#162.com>
2024-03-23 16:01:13 +0800

    feat: add husky config

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import { ref } from 'vue'
+// const a = ref<number>(0)
+</script>
 
 <style lang="less" scoped></style>
